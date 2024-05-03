c49d7c14f90a4d082906db8a4e7187d9157a7489
ambrona <mac.ambrona@gmail.com>
2024-05-03 23:20:01 +0200

    rules: improve destinies rule

--- src/rules/destinies.rs
+++ src/rules/destinies.rs
@@ -10,19 +10,27 @@ use super::{Analysis, Rule};
 #[derive(Debug)]
 pub struct DestiniesRule {
     origins_counter: usize,
+    reachable_counter: usize,
 }
 
 impl Rule for DestiniesRule {
     fn new() -> Self {
-        DestiniesRule { origins_counter: 0 }
+        DestiniesRule {
+            origins_counter: 0,
+            reachable_counter: 0,
+        }
     }
 
     fn update(&mut self, analysis: &Analysis) {
         self.origins_counter = analysis.origins.counter();
+        self.reachable_counter = analysis.reachable.counter();
     }
 
     fn is_applicable(&self, analysis: &Analysis) -> bool {
-        self.origins_counter != analysis.origins.counter() || self.origins_counter == 0
+        self.origins_counter != analysis.origins.counter()
+            || self.reachable_counter != analysis.reachable.counter()
+            || self.origins_counter == 0
+            || self.reachable_counter == 0
     }
 
     fn apply(&self, analysis: &mut Analysis) -> bool {
@@ -33,6 +41,9 @@ impl Rule for DestiniesRule {
                 let origin = analysis.origins(square).to_square();
                 progress |= analysis.update_destinies(origin, BitBoard::from_square(square))
             }
+
+            let reachable_destinies = analysis.destinies(square) & analysis.reachable(square);
+            progress |= analysis.update_destinies(square, reachable_destinies)
         }
         progress
     }
