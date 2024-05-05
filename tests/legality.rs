c855be61bed892c28b338a9d27f23b6cc186e171
ambrona <miguel.ambrona@iohk.io>
2024-05-05 18:09:28 +0200

    tests: add lib tests on tombs rule

--- tests/legality.rs
+++ tests/legality.rs
@@ -2,27 +2,31 @@ use std::str::FromStr;
 
 use chess::Board;
 
-pub enum Legality {
-    Ok,
-    Illegal,
+pub enum Legal {
+    Yes,
+    No,
     TBD, // Used for illegal positions that cannot be captured yet
 }
 
 #[test]
 fn test_legality() {
-    use crate::Legality::*;
+    use crate::Legal::*;
     [
-        ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -", Ok),
+        ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -", Yes),
         ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq -", TBD),
-        ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBNKBNR w - -", Illegal),
+        ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBNKBNR w - -", No),
+        // The following is illegal but only if 0-0 is enableld for white
+        ("rnbqkbnr/pppppp1p/8/3b4/8/6P1/PPPPPP2/RNBQK1NR w K -", No),
+        ("rn1qkbnr/pppppp1p/8/3b4/8/6P1/PPPPPP2/RNBQK1NR w K -", TBD),
+        ("rnbqkbnr/pppppp1p/8/3b4/8/6P1/PPPPPP2/RNBQK1NR w - -", Yes),
     ]
     .iter()
-    .for_each(|(fen, expected_result)| {
+    .for_each(|(fen, expected_legal)| {
         let board = Board::from_str(fen).expect("Valid Position");
         let legal = sherlock::is_legal(&board);
-        match expected_result {
-            Ok | TBD => assert!(legal),
-            Illegal => assert!(!legal),
+        match expected_legal {
+            Yes | TBD => assert!(legal),
+            No => assert!(!legal),
         }
     })
 }
