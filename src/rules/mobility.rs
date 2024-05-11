979b9b4f0d4c7eb780cd6ec0af224dec12b559b3
ambrona <miguel.ambrona@iohk.io>
2024-05-11 15:31:15 +0200

    rules: update [MobilityRule]

--- src/rules/mobility.rs
+++ src/rules/mobility.rs
@@ -2,8 +2,9 @@
 //!
 //! Based on the mobility graph, updates the information about:
 //!  - reachable_from_origin
+//!  - reachable_from_promotion
 
-use chess::{Board, Color, Rank, Square, ALL_FILES};
+use chess::{Board, Square, ALL_COLORS, ALL_FILES, PROMOTION_PIECES};
 
 use super::{Analysis, Rule};
 
@@ -31,7 +32,8 @@ impl Rule for MobilityRule {
         let mut progress = false;
 
         // update reachable_from_origin
-        for (color, rank) in [(Color::White, Rank::First), (Color::Black, Rank::Eighth)] {
+        for color in ALL_COLORS {
+            let rank = color.to_my_backrank();
             for file in ALL_FILES {
                 let square = Square::make_square(rank, file);
                 let piece = Board::default().piece_on(square).unwrap();
@@ -40,6 +42,21 @@ impl Rule for MobilityRule {
                 progress |= analysis.update_reachable_from_origin(color, file, reachable)
             }
         }
+
+        // update reachable_from_promotion
+        for color in ALL_COLORS {
+            let rank = color.to_their_backrank();
+            for piece in PROMOTION_PIECES {
+                for file in ALL_FILES {
+                    let square = Square::make_square(rank, file);
+                    let reachable = analysis.mobility.value[color.to_index()][piece.to_index()]
+                        .reachable_from_source(square);
+                    progress |=
+                        analysis.update_reachable_from_promotion(color, piece, file, reachable)
+                }
+            }
+        }
+
         progress
     }
 }
