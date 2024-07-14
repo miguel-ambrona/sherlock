cd247c8a0124eb3ee6469c1801eb324b7b6220a6
ambrona <miguel.ambrona@iohk.io>
2024-07-14 13:37:06 +0200

    WIP

--- examples/vampires.rs
+++ examples/vampires.rs
@@ -2,7 +2,7 @@ use std::{
     cmp::min,
     collections::{HashMap, HashSet},
     fs::File,
-    io::Write,
+    io::{BufRead, BufReader, Write},
     str::FromStr,
 };
 
@@ -22,45 +22,95 @@ fn preserves_clan(m: &ChessMove) -> bool {
 
 // Quick test to see if the position is legal.
 // It returns `None` if the test was not conclusive.
-fn quick_legality_test(table: &HashMap<Board, bool>, board: &Board) -> Option<bool> {
-    // If moving forwards we reach an illegal position, we are illegal.
-    for m in MoveGen::new_legal(board) {
-        let new_board = board.make_move_new(m);
-        if table.get(&new_board) == Some(&false) {
-            return Some(false);
-        }
-    }
+// fn quick_legality_test(table: &HashSet<Board>, board: &Board) -> Option<bool>
+// {     // If moving forwards we reach an illegal position, we are illegal.
+//     for m in MoveGen::new_legal(board) {
+//         let new_board = board.make_move_new(m);
+//         if table.contains(&new_board) {
+//             return Some(false);
+//         }
+//     }
 
-    // // If moving backwards we reach a legal position, we are legal.
-    // let retractable_board: RetractableBoard = (*board).into();
-    // for r in RetractionGen::new_legal(&retractable_board) {
-    //     let new_board = retractable_board.make_retraction_new(r);
-    //     let board = Board::from_str(format!("{}", new_board).as_str()).unwrap();
-    //     if table.get(&board) == Some(&true) {
-    //         return Some(true);
-    //     }
-    // }
+//     // // If moving backwards we reach a legal position, we are legal.
+//     // let retractable_board: RetractableBoard = (*board).into();
+//     // for r in RetractionGen::new_legal(&retractable_board) {
+//     //     let new_board = retractable_board.make_retraction_new(r);
+//     //     let board = Board::from_str(format!("{}",
+// new_board).as_str()).unwrap();     //     if table.get(&board) == Some(&true)
+// {     //         return Some(true);
+//     //     }
+//     // }
 
-    None
-}
+//     None
+// }
 
 fn main() {
-    let mut vampires_file = File::create("vampires-KQkq.txt").unwrap();
-    // let mut humans_file = File::create("humans-KQkq.txt").unwrap();
+    let loading = true;
 
-    // the initial position with black to move (the Head Vampire's image)
-    let board = Board::from_str("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq -").unwrap();
+    let mut vampires_file = File::create("vampires-KQkq-22.txt").unwrap();
 
-    vampires_file
-        .write_fmt(format_args!("D0 P32 {}\n", board))
-        .unwrap();
-
-    let mut table = HashMap::<Board, bool>::new();
-    let mut previous_vampire_images = vec![board];
-    let mut current_vampire_images = vec![];
+    let mut table = HashSet::<Board>::new();
+    let mut previous_vampire_images = HashSet::<Board>::new();
+    let mut current_vampire_images = HashSet::<Board>::new();
     let mut depth = 1;
 
+    let last_depth = 20;
+    let final_depth = 22;
+
+    // load existing file in memory
+    if loading {
+        let file = File::open("vampires-KQkq-20.txt").unwrap();
+
+        let reader = BufReader::new(file);
+        for line in reader.lines() {
+            let mut fen = "".to_owned();
+            let l = line.unwrap();
+            for (i, token) in l.split_ascii_whitespace().enumerate() {
+                if i == 0 {
+                    depth = token[1..].parse::<i32>().unwrap();
+                }
+                if i > 1 {
+                    fen.push_str(token);
+                    fen.push(' ');
+                }
+            }
+            let board = Board::from_str(fen.as_str()).unwrap();
+
+            table.insert(board);
+
+            if depth == last_depth {
+                previous_vampire_images.insert(board);
+            }
+        }
+        depth += 1;
+
+        println!("Loaded {} positions", table.len());
+        println!(
+            "Analyzing {} positions of {}-depth on depth {}",
+            previous_vampire_images.len(),
+            depth - 1,
+            depth
+        );
+    } else {
+        // let mut humans_file = File::create("humans-KQkq.txt").unwrap();
+
+        // the initial position with black to move (the Head Vampire's image)
+        let board =
+            Board::from_str("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq -").unwrap();
+
+        vampires_file
+            .write_fmt(format_args!("D0 P32 {}\n", board))
+            .unwrap();
+
+        previous_vampire_images.insert(board);
+        table.insert(board);
+    }
+
     loop {
+        if depth > final_depth {
+            break;
+        }
+
         if previous_vampire_images.is_empty() {
             break;
         }
@@ -82,7 +132,7 @@ fn main() {
             }
         }
 
-        let nb_cores = 64;
+        let nb_cores = 16;
         let cores: Vec<usize> = (0..nb_cores).collect();
 
         let nb_to_be_analyzed = to_be_analyzed.len();
@@ -92,17 +142,20 @@ fn main() {
         let local_tables = cores
             .par_iter()
             .map(|core_idx| {
-                let mut local_table = HashMap::<Board, bool>::new();
+                let mut local_table = HashSet::<Board>::new();
                 let start = core_idx * nb_boards_per_core;
                 let end = min((core_idx + 1) * nb_boards_per_core, nb_to_be_analyzed);
 
                 if start <= end {
                     for board in to_be_analyzed[start..end].iter() {
-                        let legal = match quick_legality_test(&table, board) {
-                            None => is_legal(board),
-                            Some(res) => res,
-                        };
-                        local_table.insert(*board, legal);
+                        // let legal = match quick_legality_test(&table, board) {
+                        // None => is_legal(board),
+                        // Some(res) => res,
+                        // };
+                        let legal = is_legal(board);
+                        if !legal {
+                            local_table.insert(*board);
+                        }
                     }
                 }
 
@@ -112,14 +165,14 @@ fn main() {
 
         for local_table in local_tables.iter() {
             table.extend(local_table.iter());
-            for (board, legal) in local_table {
-                if !legal {
-                    current_vampire_images.push(*board);
-                    let n = board.combined().popcnt();
-                    vampires_file
-                        .write_fmt(format_args!("D{} P{} {}\n", depth, n, board))
-                        .unwrap();
-                }
+            for board in local_table {
+                // if !legal {
+                current_vampire_images.insert(*board);
+                let n = board.combined().popcnt();
+                vampires_file
+                    .write_fmt(format_args!("D{} P{} {}\n", depth, n, board))
+                    .unwrap();
+                // }
                 //  else {
                 // humans_file.write_fmt(format_args!("{}\n", board)).unwrap();
                 // }
@@ -133,7 +186,7 @@ fn main() {
             current_vampire_images.len()
         );
 
-        current_vampire_images = vec![];
+        current_vampire_images = HashSet::<Board>::new();
         depth += 1;
     }
 }
